c678039540d6c080edf31090870a95079e3ff972
Mauro Vargas <maurovargas7725@gmail.com>
2025-04-14 21:49:07 -0600

    Imported FormsModule to use ngModel for form input button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { todoListItem } from '../shared/modules/todoListItem';
 import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NgFor,NgIf],
+  imports: [RouterOutlet,NgFor,NgIf,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,5 +15,10 @@ export class AppComponent {
     new todoListItem('Clean Kitchen'),
     new todoListItem('Apply for jobs')
   ];
+  todoListText : String = "";
   title = 'My Check List';
+
+  addTodoListItem(){
+    this.items.push(new todoListItem(this.todoListText))
+  }
 }
